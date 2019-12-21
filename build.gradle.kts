e2e1ae0fa443eeddee2f7a25383da9e49de4a011
Vyacheslav Chernyshoff <slava_rossii@list.ru>
2019-12-21 20:40:59 +0300

    * убрал лишние зависимости

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,12 +17,8 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
-//    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-//    testImplementation("org.springframework.boot:spring-boot-starter-test") {
-//        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
-//    }
 }
 
 tasks.withType<Test> {
